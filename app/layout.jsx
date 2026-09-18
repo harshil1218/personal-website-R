acea36406265693ae8a243919747c37ad9e08a3f
harshil1218 <harshilpatel1218@gmail.com>
2026-09-18 19:12:48 +0530

    error button design change

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./styles.css";
 import "./proposal.css";
+import "./mobile-fixes.css";
 export const metadata = { title: "A little story for Ruta", description: "A little love letter from Harshil" };
 export default function RootLayout({ children }) { return <html lang="en"><body>{children}</body></html>; }
